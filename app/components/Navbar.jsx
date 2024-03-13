5802750c2eed82e61f3f7d7b601b2ddb715e0248
KiRT4P <81580109+KiRT4P@users.noreply.github.com>
2024-03-13 08:52:24 +0100

    working mobile navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,8 +3,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
+import { IconMenu2, IconX } from '@tabler/icons-react'
+import { useState } from 'react';
 export default function Navbar() {
     const path = usePathname()
+    const [showNavbar, setShowNavbar] = useState(false)
     return (
         <div className='flex justify-center h-24 w-full z-50'>
             <nav className=" w-full h-24 fixed bg-white z-40" >
@@ -30,9 +33,33 @@ export default function Navbar() {
                             <Link href={"?dialog=y"} scroll={false}> objednať sa </Link>
                         </div>
                     </div>}
-                </div>
+                    <div onClick={e => setShowNavbar(!showNavbar)}>
+                        <IconMenu2 size={40} color='#46B8BD' />
+                    </div>
+                    {
+                        <div className="fixed md:hidden top-0 left-0  w-screen h-screen z-50  pointer-events-none">
+                            <div className={`  w-screen h-[100lvh] z-40 duration-300 ${showNavbar ? "bg-black/50 pointer-events-auto" : ""}`}>
+                                <div className={`absolute  bg-white h-[100lvh] w-[75vw] min-w-max z-50 duration-300  ${showNavbar ? "right-0" : "-right-[100vw]"}`}>
+                                    <div className="flex flex-col  items-end child:p-8 h-full mobile-navbar" onClick={e => setShowNavbar(!showNavbar)}>
+                                        <div className='!pb-4 border-b  w-2/3 flex justify-end'>
+                                            <IconX size={48} color='#46B8BD' />
+                                        </div>
+                                        <Link href={"/"}>Domov</Link>
+                                        <Link href="#sluzby">Služby</Link>
+                                        <Link href={"#onas"} >O nás</Link>
+                                        <Link href={"#foto"}>Foto</Link>
+                                        <Link href={"#kontakt"} >Kontakt</Link>
+                                        <div className="text-primary border-2 m-4 uppercase !py-3 px-6 border-primary rounded-full hover:bg-primary  hover:!text-white" data-open-modal>
+                                            <Link href={"?dialog=y"} scroll={false}> objednať sa </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-            </nav>
-        </div>
+                        </div>
+                    }
+                </div>
+            </nav >
+        </div >
     )
 }
